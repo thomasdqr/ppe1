77d9fd0e360e97bcf6ec2ed6b54e736c2709af81
Adlahmar <lahmaradel92@gmail.com>
2017-10-18 16:27:14 +0200

    php
    php+sql

--- html/index.php
+++ html/index.php
@@ -1,7 +1,43 @@
 <!DOCTYPE html>
 <?php
 if (isset($_POST['username_comptable']) AND isset($_POST['password_comptable'])) {
-	echo "WOOOOOOOORK";
+	//On a cliqué sur connexion comptable
+	$BaseDeDonnees = new PDO('mysql:host=localhost;dbname=gsb', 'root','');
+	$user = $_POST['username_comptable'];
+	$user_exist = false;
+	foreach($BaseDeDonnees->query("SELECT mdp FROM comptables WHERE utilisateur = '$user'") as $row) {
+		$user_exist = true;
+		if ($row[0]==$_POST['password_comptable']) {
+			echo 'It works';
+
+		}
+		else{
+			echo 'wrong password';
+		}
+	}
+	if($user_exist == false){
+		echo 'User doesn\'t exist';
+	}
+}
+
+
+if (isset($_POST['username_visiteur']) AND isset($_POST['password_visiteur'])) {
+	$BaseDeDonnees = new PDO('mysql:host=localhost;dbname=gsb', 'root','');
+	$user = $_POST['username_visiteur'];
+	$user_exist = false;
+	foreach($BaseDeDonnees->query("SELECT mdp FROM visiteur WHERE utilisateur = '$user'") as $row) {
+		$user_exist = true;
+		if ($row[0]==$_POST['password_visiteur']) {
+			echo 'It works';
+
+		}
+		else{
+			echo 'wrong password';
+		}
+	}
+	if($user_exist == false){
+		echo 'User doesn\'t exist';
+	}
 }
 ?>
 <html>
@@ -15,48 +51,50 @@ if (isset($_POST['username_comptable']) AND isset($_POST['password_comptable']))
 </head>
 <body>
 
-		<div>
-			<h1>GSB</h1>
+	<div>
+		<h1>GSB</h1>
+	</div>
+	<div id="divindex">
+		<h2>Connexion :</h2>
+
+		<div id="visiteur">
+			<h3>Visiteur</h3>
 		</div>
-<div id="divindex">
-	<h2>Connexion :</h2>
 
-	<div id="visiteur">
-		<h3>Visiteur</h3>
+		<div id="comptable">
+			<h4>Comptable</h4>
+		</div>
 	</div>
 
-	<div id="comptable">
-		<h4>Comptable</h4>
-	</div>
- </div>
-
- <div id="divconnexion">
-	<div id="previous">
-		<p id="retour">Retour</p>
-	</div>	
-	<img id="image" src="arrow.png">
+	<div id="divconnexion">
+		<div id="previous">
+			<p id="retour">Retour</p>
+		</div>	
+		<img id="image" src="arrow.png">
 		<p id="comptablee">Connexion comptable : </p>
-	<form action="" method="post">
-	<input name="username_comptable" placeholder="Nom d'utilisateur" >
-	<input  name="password_comptable" type="password" id="input" placeholder="●●●●●●●●●●"></input>
-	<div id="connect" onclick="document.getElementById('submit_comptable').click()">
-		<p id="connexion" >Connexion</p>
+		<form action="" method="post">
+			<input name="username_comptable" placeholder="Nom d'utilisateur" required>
+			<input  name="password_comptable" type="password" id="input" placeholder="●●●●●●●●●●" required>
+			<div id="connect" onclick="document.getElementById('submit_comptable').click()">
+				<p id="connexion" >Connexion</p>
+			</div>
+			<input type="submit" id="submit_comptable" style="display: none;">
+		</form>
 	</div>
-	<input type="submit" id="submit_comptable" style="display: none;">
-	</form>
-</div>
- <div id="divcomptable">
-	<div id="previouss">
-		<p id="retour">Retour</p>
-	</div>	
-	<img id="image" src="arrow.png">
+	<div id="divcomptable">
+		<div id="previouss">
+			<p id="retour">Retour</p>
+		</div>	
+		<img id="image" src="arrow.png">
 		<p id="comptablee">Connexion visiteur : </p>
-	<input placeholder="Nom d'utilisateur" >
-	<input  type="password" id="input" placeholder="●●●●●●●●●●"></input>
-	<div id="connect">
-		<p id="connexion" >Connexion</p>
-	</div>
-</div>
-</body>
-<script src="js/sloat.js"></script> 
-</html>
+		<form action="" method="post">
+			<input name="username_visiteur" placeholder="Nom d'utilisateur" required>
+			<input name="password_visiteur" type="password" id="input" placeholder="●●●●●●●●●●" required>
+			<div id="connect" onclick="document.getElementById('submit_visiteur').click()">
+				<p id="connexion" >Connexion</p>
+			</div>
+			<input type="submit" id="submit_visiteur" style="display: none;">
+		</div>
+	</body>
+	<script src="js/sloat.js"></script> 
+	</html>
