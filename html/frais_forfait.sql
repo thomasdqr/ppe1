d4e19c1fc88abd41d7cd14e1317ba38ccebbb490
bascia <32330833+bascia@users.noreply.github.com>
2017-11-08 14:40:57 +0100

    sql

---
+++ html/frais_forfait.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mer. 08 nov. 2017 à 13:38
+-- Version du serveur :  5.7.19
+-- Version de PHP :  5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `gsb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `frais_forfait`
+--
+
+DROP TABLE IF EXISTS `frais_forfait`;
+CREATE TABLE IF NOT EXISTS `frais_forfait` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_visiteur` int(11) NOT NULL,
+  `nuits` int(11) NOT NULL,
+  `repas` int(11) NOT NULL,
+  `kilometres` int(11) NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
