ca49b886ed6a7486899b6519e897aa18c8123229
bascia <32330833+bascia@users.noreply.github.com>
2017-11-08 17:52:14 +0100

    8/11/2017

--- html/js/interface.js
+++ html/js/interface.js
@@ -3,3 +3,13 @@ function addHF() {
 	blocks_div.innerHTML += "<div class='hf_block'></div>";
 	blocks_div.scrollTop += 100;
 }
+$('#hf_div').on('click', function(){
+	$('#arriere').css('display', 'block')
+	$('#cacher').css('display', 'block');
+$('#close').on('click', function(){
+	$('#arriere').css('display', 'none')
+	$('#cacher').css('display', 'none');
+})
+
+
+});
